abdfbd322dee15689051cb8bf366f76943f07b3a
Raul Flores <rflo.dev@gmail.com>
2023-04-26 21:59:43 -0500

    Add ExerciseAct,toolbar and back btn

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -17,7 +18,10 @@ class MainActivity : AppCompatActivity() {
         //val flStartButton : FrameLayout = findViewById(R.id.flStart)
         // using binding now. easier,faster and less code
         binding?.flStart?.setOnClickListener {
-            Toast.makeText(this,"works", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+
         }
 
 
